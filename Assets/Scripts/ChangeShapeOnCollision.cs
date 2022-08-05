3306f7ceb5e03b7ffd76b703bf3f91fcc80948ba
Xavier Travers <legoxavierlocketravers@gmail.com>
2022-08-05 13:02:04 +1000

    changed shape on collision

--- Assets/Scripts/ChangeShapeOnCollision.cs
+++ Assets/Scripts/ChangeShapeOnCollision.cs
@@ -7,17 +7,10 @@ public class ChangeShapeOnCollision : MonoBehaviour
     [SerializeField]
     private Mesh changeTo;
 
-    void Start()
-    {
-        changeTo = Resources.GetBuiltinResource<Mesh>("Sphere.fbx");
-    }
-
     private void OnCollisionEnter()
     {
-        Debug.Log("woa");
-
         // Change the shape
-        MeshFilter meshFilter = this.gameObject.GetComponent(typeof(MeshFilter)) as MeshFilter;
+        MeshFilter meshFilter = this.gameObject.GetComponent<MeshFilter>();
         meshFilter.mesh = changeTo;
     }
 }
